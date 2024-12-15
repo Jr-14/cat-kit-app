559bff5c230ec7c114c4d5fb8048a8baea17f76a
John Emerson Requizo <jr.jemerson14@gmail.com>
2024-12-15 23:34:30 +1100

    Something basic

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, TextInput, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ScrollView>
+        <Text>Some text</Text>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <Image
+            source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}}
+            style={{width: 200, height: 200}}
+          />
+          <Text>Hello world</Text>
+          <Text>Hi Alex</Text>
+          <Text>Hi John</Text>
+          <StatusBar style="auto" />
+        </View>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+          }}
+          defaultValue="You can type in me"
+        />
+      </ScrollView>
   );
 }
 
